2e5bcf152b99dabe6beec3fae83bc8924c56b285
Morshalin <morshalinislam61@gmail.com>
2020-03-13 15:41:16 +0600

    morshalin-> message replay

--- resources/views/admin/eCommerce/contact_message/replay.blade.php
+++ resources/views/admin/eCommerce/contact_message/replay.blade.php
@@ -1,15 +1,14 @@
 <div class="card">
     <div class="card-header">
-        <h6>{{_lang('Edi Production Ingredients - ')}} <span class="badge badge-primary">{{$model->name}}</span></h6>
+        <h6>{{_lang('Message Replay')}} <span class="badge badge-primary">{{$model->name}}</span></h6>
     </div>
     <div class="card-body">
-        <form action="{{route('admin.production-ingredients-category.update', $model->id)}}" method="post" id="content_form">
+        <form action="{{route('admin.eCommerce.contact-msg.store')}}" method="post" id="content_form">
             @csrf
-            @method('PATCH')
             <div class="row">
                     {{-- Production Ingredients Name --}}
                     <div class="col-md-12 form-group">
-                        <label for="name">{{_lang('Ingredients Name')}} <span class="text-danger">*</span>
+                        <label for="name">{{_lang('Customer Email')}} <span class="text-danger">*</span>
                         </label>
                         <input type="text" name="name" value="{{$model->name}}" id="name" class="form-control" placeholder="Enter Ingredients category Name" required>
                     </div>
