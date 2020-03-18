4c0457eb4692c1706b7100552982ccaffc91a087
Tariqul Islam <tariqulislamrc@pnmkhs.edu.bd>
2020-03-18 17:17:59 +0600

    migration problem solved

--- database/migrations/2020_03_18_155148_add_order_notes_to_transactions_table.php
+++ database/migrations/2020_03_18_155148_add_order_notes_to_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddPaidToTransactionsTable extends Migration
+class AddOrderNotesToTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
